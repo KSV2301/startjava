a263d52c4bb2e9da62eec64daa4ac4b85be86dd1
Kaverin Semen <kaverinsema@gmail.com>
2025-02-09 21:25:54 +0300

    Добавил файл GuessNumber.java

---
+++ GuessNumber.java
@@ -0,0 +1,17 @@
+public class GuessNumber {
+    public static void main(String[] args) {
+        int secretNumber = 52;
+        int guess = 23;
+
+        while (guess != secretNumber) {
+            if (guess < secretNumber) {
+                System.out.println(guess + " больше того, что загадал компьютер");
+                guess++;
+            } else {
+                System.out.println(guess + " меньше того, что загадал компьютер");
+                guess--;
+            }
+        }
+        System.out.println("Вы победили!");
+    }
+}
