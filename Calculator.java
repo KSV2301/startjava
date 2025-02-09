ce3dc6a778d64db367585dc7625beac616ac70fb
Kaverin Semen <kaverinsema@gmail.com>
2025-02-09 20:52:20 +0300

    Добавил файл Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public static void main(String [] args) {
+        int num1 = 2;
+        int num2 = 10;
+        char operation = '^';
+
+        int result = 0;
+
+        if (operation == '+') {
+            result = num1 + num2;
+        } else if (operation == '-') {
+            result = num1 - num2;
+        } else if (operation == '*') {
+            result = num1 * num2;
+        } else if (operation == '/') {
+            result = num1 / num2;
+        } else if (operation == '^') {
+            result = 1;
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+        } else if (operation == '%') {
+            result = num1 % num2;
+        }
+        
+        System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
+    }
+}
