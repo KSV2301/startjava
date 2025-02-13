98d3d47cd8fe3b86aa243733a468e9409d39a5be
Kaverin Semen <kaverinsema@gmail.com>
2025-02-13 16:22:35 +0300

    Обновил файл: модифицировал класс WolfT

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,71 @@
 public class Wolf {
-    String gender;
-    String name;
-    float weight;
-    int age;
-    String color;
+    private String gender;
+    private String name;
+    private float weight;
+    private int age;
+    private String color;
 
-    void walk() {
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void walk() {
         System.out.println(name + " идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println(name + " сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println(name + " бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println(name + " воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println(name + " охотится");
     }
 }
