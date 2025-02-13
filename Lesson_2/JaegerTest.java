cdba61c3e90a29d455835b2a81ae7ac0f4524ad6
Kaverin Semen <kaverinsema@gmail.com>
2025-02-13 20:42:29 +0300

    Добавил файл: JaegerTest.java

---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,15 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Gipsy Danger");
+        jaeger1.setOrigin("USA");
+        jaeger1.setSpeed(7);
+        jaeger1.setStrength(8);
+        jaeger1.setArmor(9);
+
+        Jaeger jaeger2 = new Jaeger("Cherno Alpha", "Russia", 3, 10, 10);
+
+        jaeger1.desplayInfo();
+        jaeger2.desplayInfo();
+    }
+}
