74f7de50f76daf58f2279f7b82e184965cb2edfa
Kaverin Semen <kaverinsema@gmail.com>
2025-02-13 19:33:30 +0300

    Изменил файл: исправил ошибки

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -8,10 +8,6 @@ public class Calculator {
         this.number1 = number1;
     }
 
-    public int getNumber2() {
-        return number2;
-    }
-
     public void setNumber2(int number2) {
         this.number2 = number2;
     }
