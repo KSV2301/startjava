4ee0d9d2dc480237b0d3eeb2c55344cee5ce54b9
Kaverin Semen <kaverinsema@gmail.com>
2025-02-14 18:09:55 +0300

    Добавил файл: GuessNumber.java

---
+++ Lesson_2/GuessNumber.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class GuessNumber {
+    private Player player1;
+    private Player player2;
+    private int secretNumber;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.secretNumber = new Random().nextInt(100) + 1;
+    }
+
+    public int getSecretNumber() {
+        return secretNumber;
+    }
+
+    public boolean check(int guess, Player currentPlayer) {
+        if (guess < 1 || guess > 100) {
+            System.out.println("Ошибка: введите число от 1 до 100");
+            return false;
+        }
+        if (guess > secretNumber) {
+            System.out.println("Загаданное число меньше");
+            return false;
+        } else if (guess < secretNumber) {
+            System.out.println("Загаданное число больше");
+            return false;
+        } else {
+            System.out.println(currentPlayer.getName() + " угадал число!");
+            return true;
+        }
+    }
+}
