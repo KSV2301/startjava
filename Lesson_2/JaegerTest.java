8c39b9fe547a65e3fd545cb3cf2a2aa96c51e17b
Kaverin Semen <kaverinsema@gmail.com>
2025-02-16 13:39:07 +0300

    Изменил файл: исправил ошибки

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -6,10 +6,9 @@ public class JaegerTest {
         jaeger1.setSpeed(7);
         jaeger1.setStrength(8);
         jaeger1.setArmor(9);
+        jaeger1.desplayInfo();
 
         Jaeger jaeger2 = new Jaeger("Cherno Alpha", "Russia", 3, 10, 10);
-
-        jaeger1.desplayInfo();
         jaeger2.desplayInfo();
     }
 }
