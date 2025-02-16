0cadc6794805687b9b9f5c91cd17cfb428e93e4b
Kaverin Semen <kaverinsema@gmail.com>
2025-02-16 09:31:28 +0300

    Изменил файл: исправил ошибки

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -2,7 +2,6 @@ public class Calculator {
     private int number1;
     private int number2;
     private char operation;
-    private double result;
 
     public void setNumber1(int number1) { 
         this.number1 = number1;
@@ -41,41 +40,42 @@ public class Calculator {
         }
     }
 
-    public double getResult() {
+    public double calculate() {
+        double result = 0.0;
+
         switch (operation) {
             case '+':
-                return number1 + number2;
+                result = number1 + number2;
+                break;
             case '-':
-                return number1 - number2;
+                result = number1 - number2;
+                break;
             case '*':
-                return number1 * number2;
+                result = number1 * number2;
+                break;
             case '/':
                 if (number2 == 0) {
                     System.out.println("\nОшибка: деление на ноль запрещено");
                     return 0.0;
                 }
-                return (double) number1 / number2;
+                result = (double) number1 / number2;
+                break;
             case '^':
-                double result = 1;
-                if (number2 >= 0) {
-                    for (int i = 0; i < number2; i++) {
-                        result *= number1;
-                    }
-                } else {
-                    for (int i = 0; i < -number2; i++) {
-                        result *= number1;
-                    }
+                result = 1;
+                for (int i = 0; i < Math.abs(number2); i++) {
+                    result *= number1;
+                }
+                if (number2 < 0) {
                     result = 1.0 / result;
                 }
-                return result;
+                break;
             case '%':
                 if (number2 == 0) {
                     System.out.println("\nОшибка: деление на ноль запрещено");
                     return 0.0;
                 }
-                return number1 % number2;
-            default:
-                return 0.0;
+                result = number1 % number2;
         }
+        return result;
     }
 }
