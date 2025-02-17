139fcb581cd5407c7ceaa37bf3a05c71af59c259
Kaverin Semen <kaverinsema@gmail.com>
2025-02-17 17:01:50 +0300

    Изменил файл: исправил ошибки

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -11,9 +11,30 @@ public class GuessNumber {
         this.player1 = player1;
         this.player2 = player2;
         this.scanner = scanner;
+        this.secretNumber = new Random().nextInt(101) + 1;
+    }
+
+    public void start() {
+        System.out.println("\nИгра началась!\n");
+
+        while (true) {
+            if (makeGuess(player1) || makeGuess(player2)) {
+                break;
+            }
+        }
     }
 
     private int inputNumber(Player player) {
+        while (true) {
+            int number = inputRawNumber(player);
+            if (number >= 1 || number <= 100) {
+                return number;
+            }
+            System.out.println("\nОшибка: введите число от 1 до 100\n");
+        }
+    }
+
+    private int inputRawNumber(Player player) {
         System.out.print(player.getName() + ", введите число: ");
         while (!scanner.hasNextInt()) {
             System.out.println("\nОшибка: введите число\n");
@@ -23,39 +44,16 @@ public class GuessNumber {
         return scanner.nextInt();
     }
 
-    private int inputValidNumber(Player player) {
-        int number;
-        do {
-            number = inputNumber(player);
-            if (number < 1 || number > 100) {
-                System.out.println("\nОшибка: введите число от 1 до 100\n");
-            }
-        } while (number < 1 || number > 100);
-        return number;
-    }
-
     private boolean makeGuess(Player player) {
-        int guess = inputValidNumber(player);
+        int number = inputNumber(player);
 
-        if (guess > secretNumber) {
-            System.out.println("\nЗагаданное число меньше\n");
-        } else if (guess < secretNumber) {
-            System.out.println("\nЗагаданное число больше\n");
-        } else {
+        if (number == secretNumber) {
             System.out.println("\nПоздравляем, " + player.getName() + " угадал число!");
             return true;
         }
+
+        System.out.println((number > secretNumber) ? "\nЗагаданное число меньше\n" : 
+                "\nЗагаданное число больше\n");
         return false;
-    }
-
-    public void start() {
-        secretNumber = new Random().nextInt(100) + 1;
-        System.out.println("\nИгра началась!\n");
-
-        while (true) {
-            if (makeGuess(player1) || makeGuess(player2)) {
-                break;
-            }
-        }
     }
 }
