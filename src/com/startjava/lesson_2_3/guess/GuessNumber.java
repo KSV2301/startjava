6a9d83645c52fcd8ad15f00500a0e93f7f6466b6
Kaverin Semen <kaverinsema@gmail.com>
2025-03-13 16:17:19 +0300

    Исправлен файл GuessNumber.java

--- src/com/startjava/lesson_2_3/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3/guess/GuessNumber.java
@@ -1,32 +1,59 @@
 package com.startjava.lesson_2_3.guess;
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class GuessNumberTest {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class GuessNumber {
+    private Player player1;
+    private Player player2;
+    private int secretNumber;
+    private Scanner scanner;
 
-        System.out.print("Введите имя первого игрока: ");
-        Player player1 = new Player(scanner.nextLine());
+    public GuessNumber(Player player1, Player player2, Scanner scanner) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.scanner = scanner;
+        this.secretNumber = new Random().nextInt(101) + 1;
+    }
 
-        System.out.print("Введите имя второго игрока: ");
-        Player player2 = new Player(scanner.nextLine());
+    public void start() {
+        System.out.println("\nИгра началась!\n");
 
-        String choice;
+        while (true) {
+            if (makeGuess(player1) || makeGuess(player2)) {
+                break;
+            }
+        }
+    }
 
-        do {
-            GuessNumber game = new GuessNumber(player1, player2, scanner);
-            game.start();
+    private boolean makeGuess(Player player) {
+        int number = inputNumber(player);
 
-            do {
-                System.out.print("\nХотите сыграть еще раз? [yes/no]: ");
-                choice = scanner.next().toLowerCase();
+        if (number == secretNumber) {
+            System.out.println("\nПоздравляем, " + player.getName() + " угадал число!");
+            return true;
+        }
 
-                if (!choice.equals("yes") && !choice.equals("no")) {
-                    System.out.println("\nОшибка: введите 'yes' или 'no'\n");
-                }
-            } while (!choice.equals("yes") && !choice.equals("no"));
-        } while (choice.equals("yes"));
-        System.out.println("\nИгра завершена!");
+        System.out.println((number > secretNumber) ? "\nЗагаданное число меньше\n" : 
+                "\nЗагаданное число больше\n");
+        return false;
+    }
+
+    private int inputNumber(Player player) {
+        System.out.print(player.getName() + ", введите число: ");
+        while (true) {
+            while (!scanner.hasNextInt()) {
+                System.out.println("\nОшибка: введите число\n");
+                scanner.next();
+                System.out.print(player.getName() + ", введите корректное число: ");
+            }
+            
+            int number = scanner.nextInt();
+
+            if (number >= 1 || number <= 100) {
+                return number;
+            }
+            System.out.println("\nОшибка: введите число от 1 до 100\n");
+        }
     }
 }
