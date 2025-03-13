409c7f35faa3cbb44754f30c588301ad3cbeb988
Kaverin Semen <kaverinsema@gmail.com>
2025-03-13 13:15:48 +0300

    Исправлен структура проекта

--- src/com/startjava/lesson_2_3/method/naming/MethodUtils.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lesson_2_3.method.naming;
-
-public class MethodUtils {
-    public static String getName() {
-        return Thread.currentThread().getStackTrace()[2].getMethodName();
-    }
-}
