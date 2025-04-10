6dd100d5a8e59a3a65bdc09485c610ce1d549da4
Kaverin Semen <kaverinsema@gmail.com>
2025-04-10 19:22:33 +0300

    Обновил файл SymbolTriangle.java

--- src/com/startjava/lesson_2_3_4/array/SymbolTriangle.java
+++ src/com/startjava/lesson_2_3_4/array/SymbolTriangle.java
@@ -2,41 +2,53 @@ package com.startjava.lesson_2_3_4.array;
 
 public class SymbolTriangle {
     public static void main(String[] args) {
-        printTriangle('0', '9', true);
-        printTriangle('/', '!', false);
-        printTriangle('A', 'J', false);
+        char[] symbols1 = generateSymbols('0', '9', true);
+        printTriangle(symbols1);
+
+        char[] symbols2 = generateSymbols('/', '!', false);
+        printTriangle(symbols2);
+
+        char[] symbols3 = generateSymbols('A', 'J', false);
+        printTriangle(symbols3);
+
+        char[] symbols4 = generateSymbols('z', 'a', true);
+        printTriangle(symbols4);
     }
 
-    public static void printTriangle(char left, char right, boolean ascending) {
+    private static char[] generateSymbols(char left, char right, boolean ascending) {
         if ((ascending && left > right) || (!ascending && left < right)) {
             System.out.println("Ошибка: левая граница (" + left + ") > правой (" + right + ")");
-            return;
+            return null;
         }
 
-        int height = Math.abs(right - left) + 1;
-        int symbolCount = 1;
+        int length = Math.abs(right - left) + 1;
+        char[] symbols = new char[length];
 
-        if (ascending) {
-            for (char i = left; i <= right; i++) {
-                printLine(i, symbolCount, height);
-                symbolCount += 2;
-            }
-        } else {
-            for (char i = left; i >= right; i--) {
-                printLine(i, symbolCount, height);
-                symbolCount += 2;
-            }
+        for (int i = 0; i < length; i++) {
+            symbols[i] = ascending ? (char) (left + i) : (char) (left - i);
         }
+        return symbols;
     }
 
-    public static void printLine(char symbol, int symbolCount, int totalLines) {
+    private static void printTriangle(char[] symbols) {
+        if (symbols == null) return;
+
+        int height = symbols.length;
+        int symbolCount = 1;
+
+        for (int i = 0; i < height; i++) {
+            printLine(symbols[i], symbolCount, height);
+            symbolCount += 2;
+        }
+
+        System.out.println();
+    }
+
+    private static void printLine(char symbol, int symbolCount, int totalLines) {
         int maxWidth = totalLines * 2 - 1;
         int spaces = (maxWidth - symbolCount) / 2;
 
-        StringBuilder line = new StringBuilder();
-        line.append(" ".repeat(spaces));
-        line.append(String.valueOf(symbol).repeat(symbolCount));
-
-        System.out.println(line);
+        System.out.print(" ".repeat(spaces));
+        System.out.println(String.valueOf(symbol).repeat(symbolCount));
     }
 }
