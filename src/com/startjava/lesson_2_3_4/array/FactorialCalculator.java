afb5ad0962acc03ad1ff37c90c9ffb28e749caa5
Kaverin Semen <kaverinsema@gmail.com>
2025-04-10 19:22:00 +0300

    Обновил файл FactorialCalculator.java

--- src/com/startjava/lesson_2_3_4/array/FactorialCalculator.java
+++ src/com/startjava/lesson_2_3_4/array/FactorialCalculator.java
@@ -2,68 +2,79 @@ package com.startjava.lesson_2_3_4.array;
 
 public class FactorialCalculator {
     public static void main(String[] args) {
-        calculate(null);
-        calculate(8, 0, 9);
-        calculate(-3, 1, 7, 13);
-        calculate(-22, -0);
+        int[] arr1 = {};
+        long[] res1 = calculate(arr1);
+        printFactorial(res1, arr1);
+
+        int[] arr2 = null;
+        long[] res2 = calculate(arr2);
+        printFactorial(res2, arr2);
+
+        int[] arr3 = {8, 0, 9};
+        long[] res3 = calculate(arr3);
+        printFactorial(res3, arr3);
+
+        int[] arr4 = {-3, 1, 7, 13};
+        long[] res4 = calculate(arr4);
+        printFactorial(res4, arr4);
+
+        int[] arr5 = {-22, 0};
+        long[] res5 = calculate(arr5);
+        printFactorial(res5, arr5);
     }
 
-    public static void calculate(int... values) {
-        int length = values.length;
+    private static long[] calculate(int... numbers) {
+        if (numbers == null) {
+            return null;
+        }
 
-        if (values == null || length == 0) {
+        long[] results = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            int n = numbers[i];
+            results[i] = (n < 0) ? -1 : factorial(n);
+        }
+        return results;
+    }
+
+    private static void printFactorial(long[] results, int... numbers) {
+        if (numbers == null) {
+            System.out.println("null\n");
+            return;
+        }
+
+        if (numbers.length == 0) {
             System.out.println("Массив нулевой длины\n");
             return;
         }
 
-        long[] results = new long[length];
+        for (int i = 0; i < numbers.length; i++) {
+            int n = numbers[i];
+            long result = results[i];
 
-        for (int i = 0; i < length; i++) {
-            int n = values[i];
-
-            results[i] = (n < 0) ? 1 : factorial(n);
-
-            System.out.println((n < 0) ? "Ошибка: факториал " + n + "! не определен"
-                    : n + "! = " + buildExpression(n));
+            if (result == -1) {
+                System.out.printf("Ошибка: факториал %d! не определен%n", n);
+            } else {
+                System.out.println(n == 0 || n == 1 ?
+                        n + "! = 1" :
+                        n + "! = " + buildExpression(n) + " = " + result);
+            }
         }
-
-        printResults(results);
         System.out.println();
     }
 
-    public static void printResults(long[] values) {
-        int length = values.length;
-        System.out.print("Массив результатов: [");
-        for (int i = 0; i < length; i++) {
-            System.out.print(values[i]);
-            if (i < length - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]\n");
-    }
-
-    public static String buildExpression(int n) {
-        if (n == 0 || n == 1) return "1";
-
-        long result = 1;
-        StringBuilder expression = new StringBuilder();
-
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-            expression.append(i);
-            if (i < n) {
-                expression.append(" * ");
-            }
-        }
-        return expression + " = " + result;
-    }
-
-    public static long factorial(int n) {
+    private static long factorial(int n) {
         long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
         return result;
+    }
+
+    private static String buildExpression(int n) {
+        StringBuilder sb = new StringBuilder("1");
+        for (int i = 2; i <= n; i++) {
+            sb.append(" * ").append(i);
+        }
+        return sb.toString();
     }
 }
